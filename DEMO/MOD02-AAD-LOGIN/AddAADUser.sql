982aba95a44ac5807b02f61f1cc0f8dbb54533c9
Money Yu <abc12207@gmail.com>
2023-03-14 15:52:07 +0800

    update doc and demo

--- DEMO/MOD02-AAD-LOGIN/AddAADUser.sql
+++ DEMO/MOD02-AAD-LOGIN/AddAADUser.sql
@@ -1,3 +1,7 @@
-CREATE USER [lettucebo@hotmail.com] FROM EXTERNAL PROVIDER;
+USE master
+CREATE LOGIN [lettucebo@hotmail.com] FROM EXTERNAL PROVIDER
+
+USE userdb
+CREATE USER [lettucebo@hotmail.com]  FROM EXTERNAL PROVIDER;
 
 EXEC sp_addrolemember 'db_datareader', 'lettucebo@hotmail.com';
--- DEMO/MOD03C02-ALWAYS-ENCRYPT/Query.sql
+++ DEMO/MOD03C02-ALWAYS-ENCRYPT/Query.sql
@@ -8,4 +8,10 @@ SELECT * INTO HumanResources.EmployeeAE FROM HumanResources.Employee
 
 SELECT * FROM HumanResources.EmployeeAE
 
+-- Determistc
+SELECT BirthDate FROM HumanResources.EmployeeAE GROUP BY BirthDate
+
+-- Randomized
+SELECT HireDate FROM HumanResources.EmployeeAE GROUP BY HireDate
+
 -- ACTION: Reconnect with Always Encrypted option
